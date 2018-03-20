fb17f54ec18be8accb8a6fd3edfe01f4787eabff
majorazero <daniel.cw.hsu@gmail.com>
2018-03-19 19:01:23 -0700

    added health and a damage counter

--- public/javascript/stage.js
+++ public/javascript/stage.js
@@ -2,14 +2,29 @@ var stageState = {};
 
 stageState.create = function(){
   background = game.add.sprite(0,0,"sky","sky.png");
-  star = game.add.sprite(50,game.height/2,"star","star.png");
-  var tween = game.add.tween(star)
+  player = {health:30,
+            damage:1,
+            name:"player"};
+  player.star = game.add.sprite(50,game.height/2,"star","star.png");
+  enemy = {health:10,
+            damage:1,
+            name:"notCoolBro"};
+  enemy.diamond = game.add.sprite(game.width-200,game.height/2,"diamond","diamond.png");
+  player.status = game.add.text(player.star.x,player.star.y-150, player.name + "\nhealth:" + player.health,
+                {font:'30px Courier', fill: '#fff'});
+  var tween = game.add.tween(player.star)
               .to({y:(game.height/2)-50},1000,"Linear",false)
               .to({y:(game.height/2)},1000,"Cubic",true)
               .loop(true);
-  diamond = game.add.sprite(game.width-200,game.height/2,"diamond","diamond.png");
-  var tween2 = game.add.tween(diamond)
+  player.status = game.add.text(enemy.diamond.x,enemy.diamond.y-150, enemy.name + "\nhealth:" + enemy.health,
+                {font:'30px Courier', fill: '#fff'});
+  var tween2 = game.add.tween(enemy.diamond)
               .to({y:(game.height/2)+50},1000,"Linear",false)
               .to({y:(game.height/2)},1000,"Cubic",true)
               .loop(true);
+
 };
+stageState.update = function(){
+
+
+}
